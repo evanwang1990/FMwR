25b4754ee119e690c6317c854a873ecc1bf70db7
evanwang <evanwang1990@gmail.com>
2016-07-20 23:45:58 +0800

    add dense matrix head file

--- src/util/Dmatrix.h
+++ src/util/Dmatrix.h
@@ -33,8 +33,41 @@ public:
   void setSize(uint p_dim1, uint p_dim2)
   {
     if ((p_dim1 == dim1) && (p_dim2 == dim2)) { return; }
-
+    if (value != NULL)
+    {
+      delete [] data[0]; // delete value
+      delete [] data;    // delete row pointer
+    }
+    dim1 = p_dim1;
+    dim2 = p_dim2;
+    value = new T*[dim1];
+    value[0] = new T[dim1 * dim2];
+    for (uint i = 1; i < dim1; i++) { value[1] = value[0] + dim2 * i; }
+    col_names.resize(dim2);
+    for (std::vector<std::string>::iterator it = col_names.begin(); it != col_names.end(); ++it) { *it = ""; }
   }
+
+  void init(T v)
+  {
+    for (int* p = begin(); p != end(); ++p) { *p = v; }
+  }
+
+  void assign(DMatrix<T>& v)
+  {
+    if ((dim1 != v.dim1) && (dim2 != v.dim2)) { setSize(v.dim1, v.dim2); }
+    for (T* it0 = begin(), it1 = v.begin(); it0 != end(); it0++, it ++) { *it0 = *it; }
+  }
+
+  T& operator() (uint x, uint y) { return value[x][y]; }
+
+  T operator() (uint x, uint y) const { return value[x][y]; }
+
+  T* row_iterator(uint x) const { return value[x]; }
+
+  T* begin() const { return value[0]; }
+
+  T* end() const { return value[0] + dim1 * dim2; }
+
 };
 
 #endif
