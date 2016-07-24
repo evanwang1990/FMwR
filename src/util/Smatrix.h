2c27f8abf8898524525bc6c8ecbe4b68c7313a87
evanwang <evanwang1990@gmail.com>
2016-07-25 00:12:12 +0800

    fix some bugs

--- src/util/Smatrix.h
+++ src/util/Smatrix.h
@@ -14,7 +14,7 @@ public:
   virtual void begin() = 0;
   virtual bool end() = 0;
   virtual void next() = 0;
-  virtual sparse_row<T>& get_row() = 0;
+//  virtual sparse_row<T>& get_row() = 0;
   virtual uint get_row_index() = 0;
   virtual uint nrow() = 0;
   virtual uint ncol() = 0;
@@ -23,15 +23,16 @@ public:
 
 template <typename T> class SMatrix//: public SMatrixMeta<T>
 {
-protected:
-  DVector<uint*> row_idx;
+//protected:
+public:
+  DVector<uint> row_idx;
   DVector<uint> col_idx;
   DVector<T> value;
 
 public:
   uint dim1;
   uint dim2;
-  uint64 size;
+  uint size;
 
 public:
   SMatrix()
@@ -44,41 +45,46 @@ public:
     IntegerVector dim      = _list["dim"];
     dim1                   = dim[0];
     dim2                   = dim[1];
-    size                   = (uint64)_list["size"];
+    size                   = _list["size"];
     IntegerVector row_size = _list["row_size"];
     assert((dim1 == row_size.size()) && "the length of row_size is not equal to nrow...");
     value.assign(_list["value"]);
     col_idx.assign(_list["col_idx"]);
     row_idx.setSize(dim1);
-    uint64* it = col_idx.begin();
-    for (uint i = 0; i < dim1; ++i)
+    row_idx.init(0);
+    for (uint i = 1; i < dim1; ++i)
     {
-      row_idx(i) = it;
-      it += row_size[i];
+      row_idx(i) = row_idx(i-1) + row_size(i-1);
     }
   }
 
-  T operator [] (uint i, uint j) const
+  T operator() (uint i, uint j) const
   {
-    uint* low = row_idx[i];
-    uint* high = (i == dim1) ? col_idx.end() : row_idx[i+1];
-    return search_column(low, high, j);
+    cout<<"row: "<<i<<"column = "<<j<<endl;
+    if (size == 0) { return 0.0; }
+    uint low = row_idx(i);
+    uint high = (i == dim1) ? col_idx.size() : (row_idx(i+1) - 1);
+    cout<<"~~ "<< col_idx(high) << " ~~ "<< col_idx(low)<<endl;
+    if ((j > col_idx(high)) || (j < col_idx(low))) { return 0.0; }
+    if (col_idx(low) == j) { return value(low); }
+    if (col_idx(high) == j) { return value(high); }
+    uint id = search_column(low, high, j);
+    cout<<"id = "<<id<<endl;
+    return (col_idx(id) == j) ? value(id) : 0.0;
   }
 
-  T search_column(uint* low, uint* high, uint col_idx) const
+  uint search_column(uint low, uint high, uint column) const
   {
-    if (size == 0) { return 0.0; }
-    if ((col_idx > *high) || (col_idx < *low)) { return 0.0; }
-    uint* mid;
+    uint mid;
     while (high > low)
     {
       mid = (low + high) >> 1;
-      if (*mid == col_idx) { break; }
-      if (*mid < col_idx) { low = mid; }
+      cout<<"low = "<<low<<" mid = "<<mid<<" high = "<<high<<endl;
+      if (col_idx(mid) == column) { break; }
+      if (mid < column) { low = mid; }
       else { high = mid; }
     }
-    if (*mid != col_idx) return 0.0;
-    return value[mid - col_idx.begin()];
+    return mid;
   }
 
 };
