6aa78287b7f6eb95ebf3d2c196bf1daf6e4e6091
evanwang <evanwang1990@gmail.com>
2016-07-25 14:05:29 +0800

    add iterator && fix some bugs

--- src/util/Smatrix.h
+++ src/util/Smatrix.h
@@ -23,8 +23,7 @@ public:
 
 template <typename T> class SMatrix//: public SMatrixMeta<T>
 {
-//protected:
-public:
+protected:
   DVector<uint> row_idx;
   DVector<uint> col_idx;
   DVector<T> value;
@@ -54,23 +53,21 @@ public:
     row_idx.init(0);
     for (uint i = 1; i < dim1; ++i)
     {
-      row_idx(i) = row_idx(i-1) + row_size(i-1);
+      row_idx[i] = row_idx[i-1] + row_size[i-1];
     }
   }
 
   T operator() (uint i, uint j) const
   {
-    cout<<"row: "<<i<<"column = "<<j<<endl;
-    if (size == 0) { return 0.0; }
-    uint low = row_idx(i);
-    uint high = (i == dim1) ? col_idx.size() : (row_idx(i+1) - 1);
-    cout<<"~~ "<< col_idx(high) << " ~~ "<< col_idx(low)<<endl;
-    if ((j > col_idx(high)) || (j < col_idx(low))) { return 0.0; }
-    if (col_idx(low) == j) { return value(low); }
-    if (col_idx(high) == j) { return value(high); }
+    if (size == 0) { return 0.0; } // SMatrix is empty
+    uint low = row_idx[i];
+    uint high = (i == (dim1 - 1)) ? col_idx.size() : row_idx[i+1];
+    if (high == low) { return 0.0; } // i row is empty
+    if ((j > col_idx[high-1]) || (j < col_idx[low])) { return 0.0; } // not in range
+    if (col_idx[low] == j) { return value[low]; }
+    if (col_idx[high-1] == j) { return value[high-1]; }
     uint id = search_column(low, high, j);
-    cout<<"id = "<<id<<endl;
-    return (col_idx(id) == j) ? value(id) : 0.0;
+    return (col_idx[id] == j) ? value[id] : 0.0;
   }
 
   uint search_column(uint low, uint high, uint column) const
@@ -79,12 +76,60 @@ public:
     while (high > low)
     {
       mid = (low + high) >> 1;
-      cout<<"low = "<<low<<" mid = "<<mid<<" high = "<<high<<endl;
-      if (col_idx(mid) == column) { break; }
-      if (mid < column) { low = mid; }
+      if (col_idx[mid] == column) { break; }
+      if (mid < column) { low = mid + 1; }
       else { high = mid; }
     }
     return mid;
+  }
+
+  uint nrow() const { return dim1; }
+
+  uint ncol() const { return dim2; }
+
+  uint nvalues() const { return size; }
+
+public:
+  class Iterator;
+
+};
+
+template<typename T>
+class SMatrix<T>::Iterator
+{
+protected:
+  const SMatrix<T>& matrix;
+  uint begin;
+  uint end;
+  uint pointer;
+public:
+  uint index;
+  T value;
+
+public:
+  Iterator(const SMatrix<T>& _matrix, uint row_no)
+    : matrix(_matrix)
+  {
+    begin   = matrix.row_idx[row_no];
+    end     = (row_no == (matrix.dim1 - 1)) ? matrix.size : matrix.row_idx[row_no+1];
+    pointer = begin;
+    index   = matrix.col_idx[pointer];
+    value   = matrix.value[pointer];
+  }
+
+  Iterator(const SMatrix<T>& _matrix)
+    : matrix(_matrix), begin(0), end(_matrix.size), pointer(0), index(_matrix.col_idx[0]), value(_matrix.value[0])
+  {}
+
+  ~Iterator() {}
+
+  bool is_end() { return pointer == end; }
+
+  void operator ++()
+  {
+    pointer += 1;
+    index = matrix.col_idx[pointer];
+    value = matrix.value[pointer];
   }
 
 };
