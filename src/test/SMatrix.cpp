a9680c74a83d6b3774da6e77e41a4e842ac524f8
evanwang <evanwang1990@gmail.com>
2016-07-25 14:08:10 +0800

    add SMatrix test

--- src/test/SMatrix.cpp
+++ src/test/SMatrix.cpp
@@ -4,10 +4,50 @@ using namespace std;
 
 #include "../util/Smatrix.h"
 
+/***R
+library(Rcpp)
+setwd("/home/jinlingwang/project/github/FMwR/src/test/")
+sourceCpp("../util/Swrap.cpp")
+data <- matrix(c(1, 0, 0, 0,
+                 0, 3, 4, 0,
+                 0, 0, 5, 6,
+                 0, 8, 0, 1),
+               nrow = 4,
+               byrow = T)
+sdata <- as_SMatrix(data, T)
+*/
 
 // [[Rcpp::export]]
 void test_smatrix(List x)
 {
-  SMatrix<double> y(x);
-  cout<<y.dim1<<" "<<y.size<<endl;
+  SMatrix<int> y(x);
+  // by index
+  for (uint i = 0; i < y.nrow(); ++i)
+  {
+    for (uint j = 0; j < y.ncol(); ++j)
+    {
+      cout<<y(i, j)<<" ";
+    }
+    cout<<endl;
+  }
+
+  // by Iterator
+  for (SMatrix<int>::Iterator it(y); !it.is_end(); ++it)
+  {
+    cout<<it.index<<" : "<<it.value<<" | ";
+  }
+  cout<<endl;
+
+  // by row Iterator
+  for (uint i = 0; i < y.nrow(); ++i)
+  {
+    for (SMatrix<int>::Iterator it(y, i); !it.is_end(); ++it)
+      cout<<it.index<<" : "<<it.value<<" | ";
+    cout<<endl;
+  }
+
 }
+
+/***R
+test_smatrix(sdata)
+*/
