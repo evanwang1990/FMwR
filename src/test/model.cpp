e98cb1950a90cdf92893c14a0a3f3a3677e11c3c
evanwang <evanwang1990@gmail.com>
2016-07-29 13:17:51 +0800

    test predict_batch

--- src/test/model.cpp
+++ src/test/model.cpp
@@ -1,45 +1,78 @@
 #include <Rcpp.h>
+#include <time.h>
 
 using namespace Rcpp;
 using namespace std;
 
-#include "../model.h"
+#include "../Model.h"
+
+//the main bottleneck: tranform large dense matrix to SMatrix
 
 /***R
-data <- matrix(c(1,0,0,0,
-                 0,1,0,0,
-                 0,1,1,1,
-                 1,1,1,1,
-                 0,0,1,1,
-                 1,0,0,1,
-                 2,3,4,5,
-                 10,0,9,1),
-               ncol = 4)
+data <- matrix(rep(c(1,0,0,0,
+                     0,1,0,0,
+                     0,0,1,0,
+                     1,0,0,0,
+                     0,0,1,0,
+                     1,0,0,1,
+                     2,0,0,0,
+                     0,0,0,1), 10^6),
+               ncol = 500, byrow = T)
 sdata <- as_SMatrix(data, F)
 */
 
 // [[Rcpp::export]]
-List test_model(List data)
+NumericVector model_predict_batch(List data, int threads)
 {
-  fm_model model;
+  Model model;
   model.num_factor = 3;
-  model.num_attribute = 4;
+  IntegerVector dim= data["dim"];
+  model.num_attribute = dim[1];
+  model.nthreads = threads;
   model.init();
-  List res = model.save_model();
-
+  model.w.init(0.1);
+  model.v.init(0.2);
   SMatrix<float> m(data);
-  NumericVector pred(m.nrow());
-  for (uint i = 0; i < m.nrow(); ++i)
+  DVector<double> pred(m.nrow());
+
+  for (int kk = 0; kk < 20; ++kk)
   {
-    SMatrix<float>::Iterator it(m, i);
-    pred[i] = model.predict(it);
+    Data _data;
+    _data.add_data(&m);
+    model.predict_batch(_data, pred);
   }
 
-  return List::create(res, pred);
+  return pred.to_rtype();
+}
+
+//[[Rcpp::export]]
+NumericVector model_predict_case(List data, int threads)
+{
+  Model model;
+  model.num_factor = 3;
+  IntegerVector dim= data["dim"];
+  model.num_attribute = dim[1];
+  model.nthreads = threads;
+  model.init();
+  model.w.init(0.1);
+  model.v.init(0.2);
+  SMatrix<float> m(data);
+  DVector<double> pred(m.nrow());
+
+  for (int kk = 0; kk < 20; ++kk)
+  {
+    for (uint i = 0; i < m.nrow(); ++i)
+    {
+      SMatrix<float>::Iterator it(m, i);
+      pred[i] = model.predict(it);
+    }
+  }
+
+  return pred.to_rtype();
 }
 
 /***R
-res <- test_model(sdata)
+res <- test_model(sdata, 2)
 
 predict_ <- function(x_)
 {
