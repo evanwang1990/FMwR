40eba2f5dd0337f56c13fab3ad312347f4e0944e
evanwang <evanwang1990@gmail.com>
2016-08-04 14:55:28 +0800

    add predict_batch

--- src/Model.h
+++ src/Model.h
@@ -4,9 +4,12 @@
 #include <Rcpp.h>
 #include <omp.h>
 #include <assert.h>
+#include <omp.h>
 #include "util/Dvector.h"
 #include "util/Dmatrix.h"
 #include "util/Smatrix.h"
+#include "Data.h"
+
 //TODO: 添加SOLVOE
 using namespace Rcpp;
 
@@ -21,6 +24,7 @@ public:
   DMatrixDouble v;
 
 public:
+  int nthreads; // multi-threads prediction
   uint num_attribute;
   bool k0, k1;
   uint num_factor;
@@ -29,7 +33,7 @@ public:
 
 public:
   Model()
-    : num_factor(0), init_mean(0.0), init_stdev(0.01), reg0(0.0), regw(0.0), regv(0.0), k0(true), k1(true) //rename keep_w0
+    : num_factor(0), init_mean(0.0), init_stdev(0.01), reg0(0.0), regw(0.0), regv(0.0), k0(true), k1(true), nthreads(1) //rename keep_w0
   {}
 
   ~Model() {}
@@ -45,12 +49,90 @@ public:
     m_sum_sqr.setSize(num_factor);
   }
 
-  double predict(SMatrix<float>::Iterator x)
+  double predict(SMatrix<float>::Iterator& x)
   {
     return predict(x, m_sum, m_sum_sqr);
   }
 
-  double predict(SMatrix<float>::Iterator x, DVector<double>& sum, DVector<double>& sum_sqr)
+  // void predict_batch(Data& _data, DVector<double>& _out)
+  // {
+  //   SMatrix<float>* pdata = _data.data;
+  //   uint _nr = pdata->nrow();
+  //   uint _nc = pdata->ncol();
+  //
+  //   if (_nr != _out.size()) { stop("output vector's length is not equal to number of cases..."); }
+  //   if ((w.size() != _nc) || _nc != v.ncol()) { stop("data's features not equal to parametors' number..."); }
+  //
+  //   DVector<double> sum(num_factor);
+  //   DVector<double> sum_sqr(num_factor);
+  //   #pragma omp parallel for private(sum) private(sum_sqr) num_threads(nthreads)
+  //   for (uint i = 0; i < _nr; ++i)
+  //   {
+  //     _out[i] = predict_(i, *pdata, sum, sum_sqr);
+  //   }
+  // }
+
+  void predict_batch(Data& _data, DVector<double>& _out)
+  {
+    SMatrix<float>* pdata = _data.data;
+    uint _nr = pdata->nrow();
+    uint _nc = pdata->ncol();
+
+    if (_nr != _out.size()) { stop("length of output vector is not correct..."); }
+    if ((w.size() != _nc) || _nc != v.ncol()) { stop("number of input's features is not correct..."); }
+
+    if (k0) { _out.init(w0); }
+    else { _out.init(0.0); }
+
+    uint begin, end, f, j;
+    double w_sum, v_sum, v_sum_sqr, v_res, tmp_;
+
+    if (k1)
+    {
+      #pragma omp parallel for num_threads(nthreads) private(j, end, w_sum)
+      for (uint i = 0; i < _nr; ++i)
+      {
+        end = pdata->row_idx[i+1];
+        w_sum = 0.0;
+        for (j = pdata->row_idx[i]; j < end; ++j)
+        {
+          w_sum += w[pdata->col_idx[j]] * pdata->value[j];
+        }
+        _out[i] += w_sum;
+      }
+    }
+
+    #pragma omp parallel for num_threads(nthreads) private(v_res, v_sum, v_sum_sqr, begin, end, f, j, tmp_)
+    for (uint i = 0; i < _nr; ++i)
+    {
+      end = pdata->row_idx[i+1];
+      begin = pdata->row_idx[i];
+      v_res = 0.0;
+      for (f = 0; f < num_factor; ++f)
+      {
+        v_sum = 0; v_sum_sqr = 0;
+        for (j = begin; j < end; ++j)
+        {
+          tmp_ = pdata->value[j] * v(f, pdata->col_idx[j]);
+          v_sum += tmp_;
+          v_sum_sqr += tmp_ * tmp_;
+        }
+        v_res += (0.5*v_sum*v_sum - 0.5*v_sum_sqr);
+      }
+      _out[i] += v_res;
+    }
+  }
+
+  List save_model()
+  {
+    return List::create(
+      _["w0"] = w0,
+      _["w"]  = w.to_rtype(),
+      _["v"]  = v.to_rtype()
+    );
+  }
+
+  double predict(SMatrix<float>::Iterator& x, DVector<double>& sum, DVector<double>& sum_sqr)
   {
     double pred = 0.0;
     if (k0) { pred += w0; }
@@ -78,13 +160,10 @@ public:
     return pred;
   }
 
-  List save_model()
+  double predict_(uint i, SMatrix<float>& data, DVector<double>& sum, DVector<double>& sum_sqr)
   {
-    return List::create(
-      _["w0"] = w0,
-      _["w"]  = w.to_rtype(),
-      _["v"]  = v.to_rtype()
-    );
+    SMatrix<float>::Iterator it(data, i);
+    return predict(it, sum, sum_sqr);
   }
 
 };
