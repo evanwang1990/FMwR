0e13b10c25d0789589a30f466631e770fb307088
evanwang <evanwang1990@gmail.com>
2016-08-04 14:56:33 +0800

    add some macros

---
+++ src/util/Macros.h
@@ -0,0 +1,43 @@
+#ifndef TOOL_H_
+#define TOOL_H_
+
+// some const value
+#define SQRT2PI 2.5066282746310002
+
+// TASK
+#define CLASSIFICATION 10
+#define REGRESSION     20
+#define RANKING        30
+
+// update parameters
+#define TMP(_X_) _X_##_
+#define OLD(_X_) _X_##_old
+
+#define CHECK_PARAM(_X_, _ACTION_)                             \
+{                                                              \
+  if (R_IsNaN(_X_)) {                                          \
+    _X_ = _X_##_old;                                           \
+    _ACTION_;}                                                 \
+                                                               \
+  if (_X_ == R_PosInf || _X_ == R_NegInf) {                    \
+    _X_ = _X_##_old;                                           \
+    _ACTION_;}                                                 \
+}
+
+// nan or inf counter
+#define INIT_CNTR(_X_)                                         \
+{                                                              \
+  nan_cntr_##_X_ = 0;                                          \
+  inf_cntr_##_X_ = 0;                                          \
+}
+
+#define CNTR_WARNING(_X_)                                      \
+{                                                              \
+  if (nan_cntr_##_X_ > 0 || inf_cntr_##_X_ > 0) {              \
+    cout<<"nans in "<<#_X_<<": "<<nan_cntr_##_X_<<"\t";       \
+    cout<<"infs in "<<#_X_<<": "<<inf_cntr_##_X_<<endl;       \
+  }                                                            \
+}
+
+
+#endif
