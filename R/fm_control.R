6fecebd9869421adfd6114208f267bc7daa12539
evanwang <evanwang1990@gmail.com>
2016-09-05 16:13:41 +0800

    add fm model control

---
+++ R/fm_control.R
@@ -0,0 +1,28 @@
+model.control <- function(task = "CLASSIFICATION", solver = "TDAP", nthreads = 1, ...)
+{
+  stopifnot(task %in% c("CLASSIFICATION", "REGRESSION"));
+  stopifnot(solver %in% c("MCMC", "ALS", "SGD", "FTRL", "TDAP"));
+  controls <- control_assign(model.control.default, params)
+
+  if (controls$is.default) {
+    message("Use default model settings.\n\n")
+  }
+
+  list(task = task, solver = solver, nthreads = nthreads, hyper.params = controls$contr)
+}
+
+model.control.default <- list(
+  # w0
+  keep.w0       = TRUE,
+  L2.w0         = 0.0, 
+  # w1
+  keep.w1       = TRUE,
+  L1.w1         = 0.0,
+  L2.w1         = 0.0,
+  # v
+  factor.number = 2L,
+  v.init_mean   = 0.0,
+  v.init_stdev  = 0.01,
+  L1.v          = 0.0,
+  L2.v          = 0.0
+)
