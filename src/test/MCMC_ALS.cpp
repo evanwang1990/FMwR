80e3499606bceb2a7c2ac7aeda69cdbd14ec5ec5
evanwang <evanwang1990@gmail.com>
2016-10-09 17:38:37 +0800

    .

--- src/test/MCMC_ALS.cpp
+++ src/test/MCMC_ALS.cpp
@@ -35,58 +35,105 @@ summary(pnorm(res[[2]][iris[,5] < 0]))
  res <- test(airq[,2:6], airq[,1], 1, 300, 1, 0.01); res[[1]]
  summary(pnorm(res[[2]][airq[,1] > 0]))
  summary(pnorm(res[[2]][airq[,1] < 0]))
+ res <- test(xx[["X"]], xx[["X_t"]], xx[["Y"]], 1, 300, 1, 0.01, fm_controls3, solver_controls3, validation_controls3)
  */
 
 
 #include "../util/Smatrix.h"
-#include "../Data.h"
-#include "../Model.h"
+#include "../core/Data.h"
+#include "../core/Model.h"
 #include "../util/Swrap.h"
-#include "../MCMC_ALS_Learner.h"
+#include "../solver/MCMC_ALS_Learner.h"
 
 // [[Rcpp::export]]
-List test(NumericMatrix data_, NumericVector target, int factors, int max_iter, int nthreads, double x)
+List test(List X, List X_t, NumericVector target, int factors, int max_iter, int nthreads, double x, List fm_controls, List solver_controls, List validation_controls)
 {
+  map<string, int> tasks_map;
+  tasks_map["CLASSIFICATION"] = 10;
+  tasks_map["REGRESSION"    ] = 20;
+  tasks_map["RANKING"       ] = 30;
+
+  map<string, int> solvers_map;
+  solvers_map["MCMC"] = 100;
+  solvers_map["ALS" ] = 200;
+  solvers_map["SGD" ] = 300;
+  solvers_map["FTRL"] = 400;
+  solvers_map["TDAP"] = 500;
+
+  map<string, int> evaluations_map;
+  evaluations_map["LL"  ] =  000;
+  evaluations_map["AUC" ] =  111;
+  evaluations_map["ACC" ] =  222;
+  evaluations_map["RMSE"] =  333;
+  evaluations_map["MSE" ] =  444;
+  evaluations_map["MAE" ] =  555;
+
   // init Data
-  List dl = as_SMatrix(data_, false);
-  List dlt = as_SMatrix(data_, true);
-  SMatrix<float> m(dl);
-  SMatrix<float> mt(dlt);
+  //List X = data_["X"];
+  SMatrix<float> m(X);
   Data data;
   data.add_data(&m);
-  data.add_data(&mt);
+  //List X_t = data_["X_t"];
 
   DVector<float> tg;
   tg.assign(target);
   data.add_target(&tg);
 
+
   // init Model
   Model fm;
+  List hyper_params = fm_controls["hyper.params"];
+  fm.k0            = (bool)hyper_params["keep.w0"];
+  fm.l2_reg0       = (double)hyper_params["L2.w0"];
+  fm.k1            = (bool)hyper_params["keep.w1"];
+  fm.l1_regw       = (double)hyper_params["L1.w1"];
+  fm.l2_regw       = (double)hyper_params["L2.w1"];
+  fm.num_factor    = (uint)hyper_params["factor.number"];
+  fm.init_mean     = (double)hyper_params["v.init_mean"];
+  fm.init_stdev    = (double)hyper_params["v.init_stdev"];
+  fm.l1_regv       = (double)hyper_params["L1.v"];
+  fm.l2_regv       = (double)hyper_params["L2.v"];
+  fm.nthreads      = (int)fm_controls["nthreads"];
   fm.num_attribute = data.num_features;
-  fm.nthreads = nthreads;
-  fm.k1 = true;
-  fm.num_factor = factors;
+  fm.SOLVER        = solvers_map[as<string>(fm_controls["solver"])];
+  fm.TASK          = tasks_map[as<string>(fm_controls["task"])];
   fm.init();
-  //fm.v.init(x);
-  //fm.w.init(x);
+
+  SMatrix<float> m_t;
+  if (true) {
+    m_t.assign(X_t);
+    data.add_data(&m_t);
+  }
 
   // init Metainfo
   DataMetaInfo meta(data.num_features);
 
   // init Learner
-  ALS_Learner learner;
-  //MCMC_Learner learner;
-  learner.TASK = REGRESSION;
-  learner.meta = &meta;
-  learner.fm = &fm;
-  learner.nthreads = 4;
-  learner.max_iter = max_iter;
-  learner.init();
-  learner.w_lambda.init(0.1);
-  learner.v_lambda.init(0.5);
+  Learner* learner;
+  learner = new ALS_Learner();
+  learner->meta = &meta;
+  learner->fm = &fm;
+  learner->nthreads          = (int)solver_controls["nthreads"];
+  learner->max_iter          = (int)solver_controls["max_iter"];
+  learner->tracker.step_size = (int)validation_controls["step_size"];
+  learner->tracker.max_iter  = learner->max_iter;
+  learner->type              = evaluations_map[as<string>(solver_controls["evaluate.method"])]; //TODO:收敛 R中检验回归或分类对应的评价标准
+  learner->tracker.type      = evaluations_map[as<string>(solver_controls["evaluate.method"])];
+  learner->conv_condition    = (double)solver_controls["convergence"];
+  List solver = solver_controls["solver"];
+  ((ALS_Learner*)learner)->alpha_0   = (double)solver["alpha_0"];
+  ((ALS_Learner*)learner)->gamma_0   = (double)solver["gamma_0"];
+  ((ALS_Learner*)learner)->beta_0    = (double)solver["beta_0"];
+  ((ALS_Learner*)learner)->mu_0      = (double)solver["mu_0"];
+  ((ALS_Learner*)learner)->alpha     = (double)solver["alpha"];
+  ((ALS_Learner*)learner)->w0_mean_0 = (double)solver["w0_mean_0"];
+
+
+  learner->init();
+
 
   // learn model
-  learner.learn(data);
+  learner->learn(data);
 
   // predict data
   DVector<double> res(data.num_cases);
