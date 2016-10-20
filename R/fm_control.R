ccd29d6d49e3b599c333687b5fb03ac4d2087c29
evanwang <evanwang1990@gmail.com>
2016-10-20 17:33:29 +0800

    add document

--- R/fm_control.R
+++ R/fm_control.R
@@ -1,3 +1,45 @@
+##' @title Auxiliary for Controlling FM Fitting
+##'
+##' @description
+##' Auxiliary function for \code{\link{fm}} fitting.
+##'
+##' @usage
+##' model.control(tast = "CLASSIFICATION", solver = "TDAP")
+##'
+##' @param task A character string naming the task for FM model, such as
+##' "CLASSIFICATION", "REGRESSION"
+##' @param solver A character string naming an optimization routine of FM model, the default
+##' solver is "TDAP"
+##' @param ... a list of parameters for controlling the fitting process
+##'
+##' @details
+##' FMwR provide many optimization routines including:
+##' \itemize{
+##' \item "SGD": stochastic gradient descent, supporting ℓ1 or ℓ2  \code{\link{SGD.solver}}
+##' \item "FTRL": FTRL-Proximal  \code{\link{FTRL.solver}}
+##' \item "TDAP":  \code{\link{TDAP.solver}}
+##' \item "MCMC":  \code{\link{MCMC.solver}}
+##' \item "ALS":  \code{\link{ALS.solver}}
+##' }
+##'
+##' The other parameters including:
+##' \describe{
+##' \item{\code{keep.w0}}{whether to keep intercept, default TRUE}
+##' \item{\code{L2.w0}}{regularization of intercept, default 0.0}
+##' \item{\code{keep.w1}}{whether to keep 1-way interaction, default TRUE}
+##' \item{\code{L1.w1}}{ℓ1 regularization of 1-way interaction, default 0.0}
+##' \item{\code{L2.w1}}{ℓ2 regularization of 1-way interaction, default 0.0}
+##' \item{\code{factor.number}}{number of 2-way latent factors, default 2L}
+##' \item{\code{v.init_mean}}{mean for initialization of 2-ways latent factors, default 0.0}
+##' \item{\code{v.init_stdev}}{stdev for initialization of 2-ways latent factors, default 0.01}
+##' \item{\code{L1.v}}{ℓ1 regularization of 2-way interaction, default 0.0}
+##' \item{\code{L2.v}}{ℓ2 regularization of 2-way interaction, default 0.0}
+##' }
+##'
+##' @examples
+##' model.control() # the default settings
+##'
+##'
 model.control <- function(task = "CLASSIFICATION", solver = "TDAP", ...)
 {
   stopifnot(task %in% c("CLASSIFICATION", "REGRESSION"));
--- R/solver_control.R
+++ R/solver_control.R
@@ -1,5 +1,47 @@
+##' @title Auxiliary for controlling optimization routines
+##'
+##' @description
+##' Auxiliary functions for optimization routines
+##'
+##' @usage
+##' solver.control(max_iter = 10000, nthreads = 1, convergence = 1e-4, evaluate.method = "LL", solver = TDAP.solver())
+##'
+##' @param max_iter integer giving maximal number of iterations, 25 for MCMC/ALS and 10000 for other optimization routines by default
+##'
+##' @param nthreads integer, number of threads to speed up computing, \strong{openmp} should be supported
+##'
+##' @param convergence,evaluate.method control how to early stop
+##'
+##' @param solver function to set parameters of optimization routine
+##'
+##' @details
+##' SGD/FTRL/TDAP updates model every single example, while MCMC/ALS needs to sweeps through all the data for each update.
+##' So the maximal number of iterations of MCMC/ALS is much smaller, which is 25 by default.
+##'
+##' By now, only MCMC and ALS support parallel computing. And if openmp is not supported, nthreads makes no sense.
+##'
+##' Early stopping is controlled by \strong{evaludate.method} and \strong{convergence}. If the difference of the value between two interations, which is
+##' calculated by \code{evaluate.method}, is smaller than \code{convergence} for three times in a row, iteration will stop early.
+##' \code{evaluate.method} includes:
+##' \itemize{
+##' \item "LL":loglikehood
+##' \item "AUC": auc
+##' \item "ACC": accurancy
+##' \item "RMSE": root mean square err
+##' \item "MAE": mean absolute error
+##' }
+##' "LL","AUC","ACC" are for CLASSIFICATION task. And "RMSE","MAE" are for REGRESSION task.
+##'
+##' \strong{solver} is a function to set parameters of optimization routine further, including \link{ALS.solver}
+##' \link{MCMC.solver} \link{SGD.solver} \link{TDAP.solver} \link{FTRL.solver}
+
 solver.control <- function(max_iter = 10000, nthreads = 1, convergence = 1e-4, evaluate.method = "LL", solver = TDAP.solver())
 {
+  solver_ <- deparse(substitute(solver))
+  if (grepl("MCMC|ALS", solver_)) {
+    max_iter <- 25
+  }
+
   max_threads = parallel::detectCores()
   if (nthreads >= max_threads) {
     warning("nthreads is greater than the max number of threads.\n  so nthreads will be set as ", max_threads, "!\n\n", immediate. = TRUE)
@@ -23,6 +65,14 @@ MCMC.solver.default <- list(
   w0_mean_0 = 1.0
 )
 
+
+##' @title MCMC solver
+##' @param alpha_0
+##' @param gamma_0
+##' @param beta_0
+##' @param mu_0
+##' @param alpha
+##' @param w0_mean_0
 MCMC.solver <- function(...)
 {
   controls <- control_assign(MCMC.solver.default, list(...))
@@ -44,6 +94,13 @@ ALS.solver.default <- list(
   w0_mean_0 = 1.0
 )
 
+##' @title ALS solver
+##' @param alpha_0
+##' @param gamma_0
+##' @param beta_0
+##' @param mu_0
+##' @param alpha
+##' @param w0_mean_0
 ALS.solver <- function(...)
 {
   controls <- control_assign(ALS.solver.default, list(...))
@@ -61,6 +118,10 @@ SGD.solver.default <- list(
   random_step = 1L
 )
 
+##' @title SGD solver
+##' @param learn_rate
+##' @param random_step
+##'
 SGD.solver <- function(...)
 {
   controls <- control_assign(SGD.solver.default, list(...))
@@ -80,6 +141,13 @@ FTRL.solver.default <- list(
   random_step = 1L
 )
 
+
+##' @title FTRL solver
+##' @param alpha_w
+##' @param alpha_v
+##' @param beta_w
+##' @param beta_v
+##' @param random_step
 FTRL.solver <- function(...)
 {
   controls <- control_assign(FTRL.solver.default, list(...))
@@ -98,6 +166,12 @@ TDAP.solver.default <- list(
   random_step = 1L
 )
 
+
+##' @title TDAP solver
+##' @param gamma
+##' @param alpha_w
+##' @param alpha_v
+##' @param random_step
 TDAP.solver <- function(...)
 {
   controls <- control_assign(TDAP.solver.default, list(...))
